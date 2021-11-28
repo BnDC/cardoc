22b08c8ea19a50d88ac94d19279744c3ee07d094
jh <bndgc@naver.com>
2021-11-28 15:35:53 +0900

    [Add]: signInView

--- users/views.py
+++ users/views.py
@@ -13,14 +13,14 @@ class SignUpView(View):
     def post(self, request):
         try:
             data     = json.loads(request.body)
-            email    = data['email']
-            password = data['password']
+            email    = data["email"]
+            password = data["password"]
 
             if User.objects.filter(email = email).exists():
-                return JsonResponse({"message": "DUPLICATED_EMAIL"}, status=400)
+                return JsonResponse({"message": "DUPLICATED_EMAIL"}, status = 400)
 
-            encode_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-            decode_password = encode_password.decode('utf-8')
+            encode_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+            decode_password = encode_password.decode("utf-8")
 
             User.objects.create(
                 email    = email,
@@ -28,7 +28,31 @@ class SignUpView(View):
             )
 
         except KeyError:
-            return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
         
         except JSONDecodeError:
-            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)
+            return JsonResponse({"message" : "JSON_DECODE_ERROR"}, status = 400)
+
+        return JsonResponse({"message" : "CREATED"}, status=201)
+
+class SignInView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data["email"]
+            password = data["password"]
+
+            user = User.objects.get(email = email)
+
+            if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode("utf-8")):
+                return JsonResponse({"message": "LOGIN FAILED"}, status=401)
+
+            access_token = jwt.encode({'id': user.id}, SECRET_KEY, algorithm = ALGORITHM)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+
+        except User.DoesNotExist:
+            return JsonResponse({"message": "LOGIN FAILED"}, status=401)
+
+        return JsonResponse({"access_token": access_token}, status=200)
